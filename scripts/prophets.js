5cca71a3d339a52d72054a5f6b5c5e888bc8ccf8
jclawsonOPAL <jclawsonx@mac.com>
2025-05-14 07:34:31 -0500

    basic fetch code works

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -1,15 +1,33 @@
 const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
 const cards = document.querySelector('#cards');
 
-function displayProphets(prophets) {
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        const card = document.createElement("section");
+        const fullName = document.createElement("h2");
+        const portrait = document.createElement("img");
 
+        const prophetName = `${prophet.name} ${prophet.lastname}`;
+
+        fullName.textContent = prophetName;
+
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", `Portrait of ${prophetName}`);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "362");
+        portrait.setAttribute("height", "447");
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    });
 }
 
 async function getProphetData() {
-  const response = await fetch(url);
-  const data = await response.json();
-  console.table(data.prophets);
-  displayProphets(data.prophets);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.prophets);
+    displayProphets(data.prophets);
 }
 
 getProphetData();
