7e07b0a630db7ea3bc23b2767aa9824fa3a31b06
jclawsonOPAL <jclawsonx@mac.com>
2025-05-16 07:49:11 -0500

    working on mode switch

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,8 +1,20 @@
 const url = "data/members.json";
 const cards = document.querySelector('main');
+const displayBusinessStyle = document.querySelector('#business_display_style');
+const mode_switch_button = document.querySelector("#mode_switch_button");
 const numberPattern = /\d+/g;
 
-const displayBusinesses = (members) => {
+mode_switch_button.addEventListener('click', () => {
+    if (displayBusinessStyle.value == "card") {
+        displayBusinessStyle.value = "list";
+    } else if (displayBusinessStyle.value == "list") {
+        displayBusinessStyle.value = "card";
+    }
+   
+    getBusinessData(displayBusinessStyle);
+});
+
+const displayBusinessCards = (members) => {
     members.forEach((member) => {
         const card = document.createElement("section");
         const companyName = document.createElement("h2");
@@ -72,12 +84,82 @@ const displayBusinesses = (members) => {
     });
 }
 
-async function getBusinessData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.table(data.members);
-    displayBusinesses(data.members);
+const displayBusinessList = (members) => {
+    members.forEach((member) => {
+        const card = document.createElement("section");
+        const companyName = document.createElement("h2");
+        const tagline = document.createElement("h3");
+
+        companyName.textContent = member.name;
+        tagline.textContent = member.tag_line;
+
+        const businessInfoContainer = document.createElement("div");
+        const businessDetailsContainer = document.createElement("div");
+
+        businessInfoContainer.className = "business_info_container";
+        businessDetailsContainer.className = "business_details_container";
+
+        const p1 = document.createElement("p");
+        const p2 = document.createElement("p");
+        const p3 = document.createElement("p");
+
+        const s1 = document.createElement("span");
+        const s2 = document.createElement("span");
+        const s3 = document.createElement("span");
+
+        s1.textContent = "EMAIL: ";
+        s2.textContent = "PHONE: ";
+        s3.textContent = "URL: ";
+
+        const a1 = document.createElement("a");
+        const a2 = document.createElement("a");
+        const a3 = document.createElement("a");
+
+        a1.setAttribute("href", `mailto:${member.company_image_url}`);
+        a2.setAttribute("href", `tel:+${member.phone_number}`);
+        a3.setAttribute("href", `https://${member.company_image_url}`);
+
+        a1.textContent = member.email;
+        a2.textContent = member.phone_number.match( numberPattern );
+        a3.textContent = member.website_url;
+
+        p1.appendChild(s1);
+        p1.appendChild(a1);
+
+        p2.appendChild(s2);
+        p2.appendChild(a2);
+
+        p3.appendChild(s3);
+        p3.appendChild(a3);
+
+        businessDetailsContainer.appendChild(p1);
+        businessDetailsContainer.appendChild(p2);
+        businessDetailsContainer.appendChild(p3);
+
+        businessInfoContainer.appendChild(businessDetailsContainer);
+
+        card.appendChild(companyName);
+        card.appendChild(tagline);
+        card.appendChild(businessInfoContainer);
+        cards.appendChild(card);
+    });
 }
 
-getBusinessData();
+const getBusinessData = async (displayBusinessStyleValue) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.table(data.members);
+        // console.log(displayBusinessStyle.value)
 
+        if (displayBusinessStyleValue.value == "card") {
+            displayBusinessCards(data.members);
+        } else if (displayBusinessStyleValue.value == "list") {
+            displayBusinessList(data.members);
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error); // Handle any errors    
+    }
+}
+
+getBusinessData(displayBusinessStyle);
--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -7,47 +7,34 @@ hamButton.addEventListener('click', () => {
 });
 
 const homeButton = document.querySelector('#home');
-const chamberButton = document.querySelector('#chamber');
-const finalButton = document.querySelector('#final');
-const githubProfileButton = document.querySelector('#github_profile');
-const linkedinButton = document.querySelector('#linkedin');
+const discoveryButton = document.querySelector('#discovery');
+const directoryButton = document.querySelector('#directory');
+const joinProfileButton = document.querySelector('#join');
 
 homeButton.addEventListener('click', () => {
     homeButton.classList.add("active");
-    chamberButton.classList.remove("active");
-    finalButton.classList.remove("active");
-    githubProfileButton.classList.remove("active");
-    linkedinButton.classList.remove("active");
+    discoveryButton.classList.remove("active");
+    directoryButton.classList.remove("active");
+    joinProfileButton.classList.remove("active");
 });
 
-chamberButton.addEventListener('click', () => {
+discoveryButton.addEventListener('click', () => {
     homeButton.classList.remove("active");
-    chamberButton.classList.add("active");
-    finalButton.classList.remove("active");
-    githubProfileButton.classList.remove("active");
-    linkedinButton.classList.remove("active");
+    discoveryButton.classList.add("active");
+    directoryButton.classList.remove("active");
+    joinProfileButton.classList.remove("active");
 });
 
-finalButton.addEventListener('click', () => {
+directoryButton.addEventListener('click', () => {
     homeButton.classList.remove("active");
-    chamberButton.classList.remove("active");
-    finalButton.classList.add("active");
-    githubProfileButton.classList.remove("active");
-    linkedinButton.classList.remove("active");
+    discoveryButton.classList.remove("active");
+    directoryButton.classList.add("active");
+    joinProfileButton.classList.remove("active");
 });
 
-githubProfileButton.addEventListener('click', () => {
+joinProfileButton.addEventListener('click', () => {
     homeButton.classList.remove("active");
-    chamberButton.classList.remove("active");
-    finalButton.classList.remove("active");
-    githubProfileButton.classList.add("active");
-    linkedinButton.classList.remove("active");
-});
-
-linkedinButton.addEventListener('click', () => {
-    homeButton.classList.remove("active");
-    chamberButton.classList.remove("active");
-    finalButton.classList.remove("active");
-    githubProfileButton.classList.remove("active");
-    linkedinButton.classList.add("active");
+    discoveryButton.classList.remove("active");
+    directoryButton.classList.remove("active");
+    joinProfileButton.classList.add("active");
 });
