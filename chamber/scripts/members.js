c89626fcfd3072abd62ed3ede647783338317b8c
jclawsonOPAL <jclawsonx@mac.com>
2025-05-18 16:46:12 -0500

    added tab index

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -29,11 +29,14 @@ const displayLogoCards = (members) => {
         companyLogo.setAttribute("width", "180");
         companyLogo.setAttribute("height", "180");
         companyLogo.setAttribute("loading", "lazy");
+        companyLogo.setAttribute("tabindex", "0");
 
         const p1 = document.createElement("p");
         const p2 = document.createElement("p");
         const p3 = document.createElement("p");
         const p4 = document.createElement("p");
+
+        p1.setAttribute("tabindex", "0");
 
         p1.textContent = `${member.address.street1} ${member.address.city} ${member.address.state} ${member.address.zip}`;
 
@@ -42,8 +45,13 @@ const displayLogoCards = (members) => {
         const a4 = document.createElement("a");
 
         a2.setAttribute("href", `mailto:${member.company_image_url}`);
+        a2.setAttribute("tabindex", "0");
+
         a3.setAttribute("href", `tel:+${member.phone_number.match( numberPattern )}`);
+        a3.setAttribute("tabindex", "0");
+
         a4.setAttribute("href", `https://${member.company_image_url}`);
+        a4.setAttribute("tabindex", "0");
 
         a2.textContent = member.email;
         a3.textContent = member.phone_number;
@@ -93,11 +101,17 @@ const displayBusinessList = (members) => {
         phoneNumber.className = "phone_number";
         homepage.className = "website";
 
+        companyName.setAttribute("tabindex", "0");
+        address.setAttribute("tabindex", "0");
+
         const a3 = document.createElement("a");
         const a4 = document.createElement("a");
 
         a3.setAttribute("href", `tel:+${member.phone_number.match( numberPattern )}`);
+        a3.setAttribute("tabindex", "0");
+
         a4.setAttribute("href", `https://${member.company_image_url}`);
+        a4.setAttribute("tabindex", "0");
 
         a3.textContent = member.phone_number;
         a4.textContent = member.website_url;
