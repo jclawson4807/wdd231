0b068648aaaf1bb8138f9493e67e55fe9d8fcef6
jclawsonOPAL <jclawsonx@mac.com>
2025-05-20 13:02:07 -0500

    finished weather project

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,11 +2,27 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const figCaption = document.querySelector('figcaption');
 
+const currentTempSpan = document.querySelector("#current-temp");
+const weatherIconImg = document.querySelector("#weather-icon");
+
 const apiKey = "34d341efa6a94c1b8066b3239d6203e1";
 const lat = "49.75";
 const lon = "6.64";
 
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+
+function displayResults(data) {
+    currentTempSpan.innerHTML = `${data.main.temp}&deg;F`;
+    const description = data.weather[0].description;
+    const icon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+    weatherIconImg.setAttribute("src", icon);
+    weatherIconImg.setAttribute("alt", description);
+    weatherIconImg.setAttribute("width", 50);
+    weatherIconImg.setAttribute("height", 50);
+
+    figCaption.textContent = description;
+}
 
 const apiFetch = async () => {
     try {
@@ -15,7 +31,7 @@ const apiFetch = async () => {
                 const data = await response.json();
 
                 console.log(data); // testing only
-                // displayResults(data); // uncomment when ready
+                displayResults(data); // uncomment when ready
             } else {
                 throw Error(await response.text());
             }
