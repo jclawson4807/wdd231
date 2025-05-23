969c35207b62977d6576fa3a461424703c4a1628
jclawsonOPAL <jclawsonx@mac.com>
2025-05-22 19:51:48 -0500

    working on current weather card

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,18 +1,18 @@
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const figCaption = document.querySelector('figcaption');
-
-const currentTempSpan = document.querySelector("#current-temp");
+const currentDateTempSpan = document.querySelector("#current-date-temp");
+const currentDateHighTempSpan = document.querySelector("#current-date-high-temp");
+const currentDateLowTempSpan = document.querySelector("#current-date-low-temp");
 const weatherIconImg = document.querySelector("#weather-icon");
 
 const apiKey = "34d341efa6a94c1b8066b3239d6203e1";
-const lat = "49.75";
-const lon = "6.64";
+const lat = "43.187640";
+const lon = "-89.236768";
 
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
 
 function displayResults(data) {
-    currentTempSpan.innerHTML = `${data.main.temp}&deg;F`;
+    currentDateTempSpan.innerHTML = `${data.main.temp}&deg;F`;
+    currentDateHighTempSpan.innerHTML = `${data.main.temp_max}&deg;F`;
+    currentDateLowTempSpan.innerHTML = `${data.main.temp_min}&deg;F`;
     const description = data.weather[0].description;
     const icon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
 
@@ -21,12 +21,12 @@ function displayResults(data) {
     weatherIconImg.setAttribute("width", 50);
     weatherIconImg.setAttribute("height", 50);
 
-    figCaption.textContent = description;
+    // figCaption.textContent = description;
 }
 
 const apiFetch = async () => {
     try {
-        const response = await fetch(url);
+        const response = await fetch(weatherUrl);
             if (response.ok) {
                 const data = await response.json();
 
